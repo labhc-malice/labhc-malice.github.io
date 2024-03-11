74781f8cefd23ec3560a525a08559d89b5dca12f
Rémi Emonet <remi.emonet@reverse--com.heeere>
2024-03-11 10:06:31 +0100

    Add diff after labmetry_get

--- bin/labmetry_get.sh
+++ bin/labmetry_get.sh
@@ -9,4 +9,4 @@ s() {
 year=$(date +%Y)
 wget 'https://labh-curien.univ-st-etienne.fr/labmetry/api/export_publications_team/2023/'$year'/1/12/all/A/malice?giveJournals=1&giveConferences=1&giveOthers=1&format=html&sort=chronological&apikey='$(s 0428 1919 2301)'d8ecfc5eea4ed2b07c4ae912d6' -q -O - | grep -v '<h1>.*</h1>' > _includes/labmetry_raw.html
 
-
+git diff _includes/labmetry_raw.html
